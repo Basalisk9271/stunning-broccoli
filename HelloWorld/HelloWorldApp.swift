c0ca5390fe7cd48dade53782af5c2637cb04de24
Gabe Imlay <gabe.imlay9271@gmail.com>
2023-02-01 19:47:40 -0500

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Gabe Imlay on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
